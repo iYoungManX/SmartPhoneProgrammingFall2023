bf5336fdb64f726f19b1bc5b5da9714b45cca40c
iYoungManX <yao.zo@northeastern.edu>
2023-10-28 18:15:13 -0700

    StockApp

---
+++ StockApp2/StockApp2/Utils/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  StockApp2
+//
+//  Created by 姚宗辉 on 10/21/23.
+//
+
+import Foundation
+
+let apiKey = "48d4dc7037059a704102ea318efa96d6"
+let baseURL = "https://financialmodelingprep.com/api/v3/"
+
